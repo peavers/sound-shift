4e1a200a5f7d48cb771bb717a7eb254c4be78413
Chris Turner <peavers@gmail.com>
2025-12-14 13:54:27 -0600

    fix: Remove self updater for now

--- desktop/src-tauri/src/lib.rs
+++ desktop/src-tauri/src/lib.rs
@@ -17,7 +17,6 @@ use state::{AppSettings, AppState, DeviceGroup};
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_autostart::init(
